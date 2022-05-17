e197195c82dc73f01044259aa4e506a32d866312
diego-pedro <diego.pedro96@gmail.com>
2022-05-17 19:50:11 -0300

    Implemented API requests and playlist responses

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-# API Request Info
-SpotifyAPIClientId = "08c1a6be652e4fdca07f1815bfd167e4"
+""" API Request Info """
+
+# Spotify
+SPOTIPY_CLIENT_ID = "2bf475fed7634cbc937836b2336fbcfb"
+SPOTIPY_CLIENT_SECRET = "229841e1ef574dbfb403b5fc2774005e"
+SPOTIPY_REDIRECT_URI = "http://example.com"
+
+SpotifyEndpointUrl = "https://api.spotify.com/v1/search"
+
+
+# Create Spotipy instance for tracks search function.
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                                               client_secret=SPOTIPY_CLIENT_SECRET,
+                                               redirect_uri=SPOTIPY_REDIRECT_URI))
+
+# Open Weather
 OpenWeatherMapsKey = "b77e07f479efe92156376a8b07640ced"
+OpenWeatherUrl = "http://api.openweathermap.org/data/2.5/weather"
 
+# Flask Server Setup
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
 """
 -> The Challenge
@@ -61,8 +81,88 @@ OpenWeatherMapsKey = "b77e07f479efe92156376a8b07640ced"
 
 """
 
-# TODO 1: Route for Rest API Main Landing Page
-# TODO 2: Route for Request Given City or LAT/LNG Parameters
-# TODO 3: Use Returned Weather (Temperature converted to Celsius) Response to Identify Song Genre
-# View the conditions above
-# TODO 4: Send Request to Spotify and Return List of Track Names in that Genre
+
+# Route for Rest API Main Landing Page
+@app.route("/")
+def home():
+    return render_template(template_name_or_list="index.html")
+
+
+# Function to get the genre for the search in spotify.
+def get_genre(temp_celsius):
+    """
+    Given a temperature in Celsius, returns the genre.
+    """
+    if temp_celsius > 30.0:
+        return "party"
+    elif 15 < temp_celsius < 30:
+        return "pop"
+    elif 15 > temp_celsius > 10:
+        return "rock"
+    else:
+        return "classic"
+
+
+# Routes for Request Given City or LAT/LNG Parameters
+
+# City as a Request Param
+@app.route("/playlist/c=<city>")
+def playlist_by_city_climate(city):
+    request_params = {
+        "q": city,
+        "appid": OpenWeatherMapsKey
+    }
+    weather_response = requests.get(url=OpenWeatherUrl, params=request_params)
+
+    # TODO: Re-route to error page or api command info
+    if weather_response.status_code != 200:
+        return render_template(template_name_or_list="index.html")
+
+    weather_content = weather_response.json()
+
+    t_celsius = round(weather_content["main"]["temp"] - 273.15, 2)
+    genre = get_genre(t_celsius)
+
+    search_result = sp.search(q=f'genre: {genre}')
+    song_list = []
+
+    for item in search_result['tracks']['items']:
+        if item['name'] not in song_list:
+            song_list.append(item['name'])
+
+    return render_template(template_name_or_list="playlist.html", temp=t_celsius, location=city.title(),
+                           genre=genre.title(), songs=song_list)
+
+
+# Latitude and Longitude as a Request Param
+@app.route("/playlist/lat=<lat>&lon=<lon>")
+def playlist_by_lat_lon_climate(lat, lon):
+    request_params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": OpenWeatherMapsKey
+    }
+    weather_response = requests.get(url=OpenWeatherUrl, params=request_params)
+
+    # TODO: Re-route to error page or api command info
+    if weather_response.status_code != 200:
+        return render_template(template_name_or_list="index.html")
+
+    weather_content = weather_response.json()
+
+    t_celsius = round(weather_content["main"]["temp"] - 273.15, 2)
+    genre = get_genre(t_celsius)
+
+    search_result = sp.search(q=f'genre: {genre}')
+    song_list = []
+
+    for item in search_result['tracks']['items']:
+        if item['name'] not in song_list:
+            song_list.append(item['name'])
+
+    return render_template(template_name_or_list="playlist.html", temp=t_celsius, location=f'{lat}ºN {lon}ºL',
+                           genre=genre.title(), songs=song_list)
+
+
+if __name__ == "__main__":
+    app.run()
